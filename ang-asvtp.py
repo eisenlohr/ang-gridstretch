ec0a9a7e53bfc011af582d391799c45a38c6a744
Philip Eisenlohr <eisenlohr@egr.msu.edu>
2022-03-31 13:01:46 -0400

    symmetry option; robust Euler angle import

--- ang-asvtp.py
+++ ang-asvtp.py
@@ -8,9 +8,14 @@ import damask
 
 from pathlib import Path
 
-parser = argparse.ArgumentParser(description='transform orientation imaging map in ANG format (TSL/EDAX) to VTP.')
+parser = argparse.ArgumentParser(description='transform orientation imaging map in ANG format (TSL/EDAX) to VTP.',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--ipf',nargs=3,default=[0,0,1],type=float,metavar='float',
                     help='Sample direction of inverse pole figure')
+parser.add_argument('--symmetry',
+                    choices=set(damask._crystal.lattice_symmetries.values()),
+                    default=list(damask._crystal.lattice_symmetries.values())[-1],
+                    help='Lattice symmetry')
 parser.add_argument('file',
                     help='ANG file to transform to VTP')
 
@@ -20,7 +25,8 @@ file = Path(args.file)
 t = damask.Table.load_ang(file)
 
 mesh = pv.PolyData(np.column_stack((t.get('pos'),np.zeros(len(t)))))
-mesh['IPF'] = damask.Orientation.from_Euler_angles(phi=t.get('eu'),family='cubic').IPF_color(args.ipf)
+mesh['IPF'] = damask.Orientation.from_Euler_angles(phi=t.get('eu')%(np.pi*np.array([2,1,2])),
+                                                   family=args.symmetry).IPF_color(args.ipf)
 for l in set(t.labels) - {'pos'}:
     mesh[l] = t.get(l)
 
